98d4ec6222bd6ef3a6f87bc5646905f3e1ed48fa
Jatin Motwani <jatinmotwani17@gmail.com>
2025-11-16 19:37:12 +0530

    Remove ig

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { FaInstagram, FaLinkedin } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
 import { SiGmail } from "react-icons/si";
 
 const Footer = () => {
   return (
 <footer className="fixed bottom-0 left-0 w-full py-4 bg-black-100/55 text-white flex justify-center gap-6 z-50">      <a
-        href="https://www.instagram.com/jatin_motwani_07?igsh=cjA0MHlkZjRieHk4&utm_source=qr"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaInstagram size={24} />
-      </a>
-      <a
         href="mailto:jatinmotwnai17@gmail.com"
         target="_blank"
         rel="noopener noreferrer"
